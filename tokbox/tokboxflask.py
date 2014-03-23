2e77d42b60071e8db0d6de19a1090c341cc8bf7e
Brian Chuk <brian_chuk@yahoo.com>
2014-03-22 20:22:41 -0400

    more tokbox

---
+++ tokbox/tokboxflask.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello(name=None):
+    page = render_template('tokbox.html', name=name)
+    return page
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run(host="0.0.0.0",port=1337)
